05765502c0677824d10b9c85f348e65da836b7b0
efeglr <257828430+efeglr@users.noreply.github.com>
2026-06-15 10:25:10 +0200

    Add: Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Nav from './components/Nav.jsx';
+import Footer from './components/Footer.jsx';
 
 const App = () => {
   return (
     <>
       <Nav />
+      <Footer />
     </>
   );
 };
