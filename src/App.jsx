b9693e8535feecf3647e62188d1092edd5054dee
Bashar Alwarad <beelwarad52@gmail.com>
2026-06-15 10:13:00 +0200

    add nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Nav from './components/Nav.jsx';
+
 const App = () => {
   return (
-    <div>
-      <button className="bg-red-200 m-1 p-1">click me</button>
-    </div>
+    <>
+      <Nav />
+    </>
   );
 };
 
